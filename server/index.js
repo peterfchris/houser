719ebf3546b49f3a42e2b823018f1af0e2db595d
Peter Christensen <peter.f.chris@gmail.com>
2019-05-24 11:03:51 -0600

    database and server setup

---
+++ server/index.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const express = require('express')
+const massive = require('massive')
+const app = express()
+app.use(express.json())
+const {SERVER_PORT, CONNECTION_STRING} = process.env
+const controller = require('./controller')
+
+massive(CONNECTION_STRING).then((database) => {
+    app.set('db', database)
+    console.log(`database set!`)
+    app.listen(SERVER_PORT, () => console.log(`Nothing is screwing up your sleep on port: ${SERVER_PORT}` ))
+})
+
+// Endpoints here
